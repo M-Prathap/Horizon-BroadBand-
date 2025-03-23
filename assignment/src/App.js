6813b9a9d94b70f3889dd2b9fb7bb7bfe49d0fc6
Prathap-Muruga <prathap110201@gmail.com>
2025-03-23 20:31:01 +0530

    first-commits

---
+++ assignment/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="common-box">
+        <div className="box">
+          <div className="red-square top-left"></div>
+          <div className="red-square bottom-left"></div>
+          <div className="black-square top-right"></div>
+          <div className="black-square bottom-right"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
